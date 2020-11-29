f366b3f0bf66960885a53e99f766b2f3cfc2855e
Marcin Doliwa <mdoliwa@gmail.com>
2020-11-29 14:41:29 +0100

    Draw board with players cells

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ const cellSize = 32;
 const rows = 16;
 const columns = rows * 2;
 
+const playerOneCells = [[0,0], [0,1], [0,2]];
+const playerTwoCells = [[1,1], [2,2], [3,3]];
+
+var board = initBoard(playerOneCells, playerTwoCells);
+
 canvas.width = cellSize * columns + 1;
 canvas.height = cellSize * rows + 1; 
 
@@ -24,12 +29,16 @@ for (let y = 0.5; y <= cellSize * rows + 0.5; y += cellSize) {
 	ctx.stroke();
 }
 
+board.forEach((cell) => setCell(cell[0], cell[1]));
+
 canvas.addEventListener('click', function(e) {
 	const rect = canvas.getBoundingClientRect();
 	const x = e.clientX - rect.left, y = e.clientY - rect.top;
 
 	const column = Math.floor(x / cellSize);
 	const row = Math.floor(y / cellSize);
+
+	if (row >= rows || column >= columns) { return }
 
 	setCell(column, row)
 })
@@ -38,4 +47,8 @@ function setCell(x, y) {
 	ctx.beginPath();
 	ctx.rect(x * cellSize + 1, y * cellSize + 1, cellSize - 1, cellSize - 1);
 	ctx.fill();
+}
+
+function initBoard(playerOneCells, playerTwoCells) {
+	return playerOneCells.concat(playerTwoCells.map((cell) => [cell[0] + rows, cell[1]]));
 }
